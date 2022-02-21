d79509ae687477e07964fdf2e62eaaf8dc46a702
amisdun <michealdunamis@gmail.com>
2022-02-21 11:52:24 +0000

    amisdun: service to handle webhook events

---
+++ src/services/handleWebHookEvent.js
@@ -0,0 +1,21 @@
+import { WebHookError } from "../utils/index.js";
+import { handleMessage, handlePostBack } from "../services/index.js";
+
+export const handleWebHookEvent = async (body) => {
+	const { object } = body;
+	if (object === "page") {
+		body.entry.forEach(async function (entry) {
+			entry.messaging.forEach(async function (event) {
+				const senderId = event.sender.id;
+				if (event?.message) {
+					await handleMessage(senderId, event.message);
+				}
+				if (event?.postback) {
+					await handlePostBack(senderId, event.postback);
+				}
+			});
+		});
+		return "EVENT_RECEIVED";
+	}
+	throw new WebHookError("Not Found", 404);
+};
