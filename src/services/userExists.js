3ea5961c4adeba7b11b6f325ef31f824ef3b5138
amisdun <michealdunamis@gmail.com>
2022-02-21 11:53:02 +0000

    amisdun: service to check if a user exist

---
+++ src/services/userExists.js
@@ -0,0 +1,9 @@
+import { User } from "../models/index.js";
+
+export const userExists = async (userId) => {
+	const user = await User.findOne({ user: userId });
+	if (user && user?._id) {
+		return true;
+	}
+	return false;
+};
