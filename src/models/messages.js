c69ebacf73d498cddc823434ebeef48ebd550d88
amisdun <michealdunamis@gmail.com>
2022-02-21 11:49:41 +0000

    amisdun: created file to define message models

---
+++ src/models/messages.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const MessageSchema = new mongoose.Schema({
+	message: {
+		type: String,
+		required: true,
+	},
+});
+
+const Message = mongoose.model("Message", MessageSchema);
+
+export { Message };
